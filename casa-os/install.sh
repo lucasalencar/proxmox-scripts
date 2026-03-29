da0029e099303003c7bf93bd384fd39db085ab7d
Lucas Alencar <alencar.lucas.a@gmail.com>
2026-03-29 13:13:37 -0300

    Configure mounts in specific install scripts

--- casa-os/install.sh
+++ casa-os/install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-# Install CasaOS using LXC container
+echo "Install CasaOS using LXC container"
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/ct/casaos.sh)"
+
+echo "Setting up mount: /tank/data/memorias -> /Gallery (mp0)"
+pct set "$container_id" -mp1 /tank/data/memorias,mp=/DATA/Gallery
+
+echo "Setting up mount: /tank/data/media -> /Media (mp1)"
+pct set "$container_id" -mp2 /tank/data/media,mp=/DATA/Media
