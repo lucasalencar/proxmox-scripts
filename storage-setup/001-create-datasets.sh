7e5c0e157510d1b6e7faf8d447193a3996c77ba9
Lucas Alencar <alencar.lucas.a@gmail.com>
2026-03-29 11:51:16 -0300

    Document create datasets script

--- storage-setup/001-create-datasets.sh
+++ storage-setup/001-create-datasets.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# This script automates the creation of the storage structure on the ZFS Pool 'tank'.
+# It creates the main datasets for media and memories, organizes subfolders,
+# and sets up permissions (UID 100000) so Proxmox containers (LXC) can
+# read and write to files natively, ensuring performance and isolation.
 
 # Datasets controlled by ZFS
 zfs create tank/data # Main
@@ -12,6 +16,5 @@ mkdir -p /tank/data/media/filmes /tank/data/media/series
 chown -R 100000:100000 /tank/data # Change ownership to default containers user
 chmod -R 775 /tank/data
 
-
-# List of all datastes created
+# List all created datasets
 zfs list
